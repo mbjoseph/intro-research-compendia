a913370947b00eda9704660d0e9fdbfdc0b36d57
Mike Nosal <mnosal@mitre.org>
2022-02-15 17:34:27 -0500

    Update 02-clean_data.py

--- example-compendium/analysis/02-clean_data.py
+++ example-compendium/analysis/02-clean_data.py
@@ -7,11 +7,11 @@ data_path = Path("data/raw/data.csv")
 d = pd.read_csv(data_path, low_memory=False)
 
 # get number of accidents per day
-d['occurrence_date'] = pd.to_datetime(d['FIRST_OCCURRENCE_DATE']).dt.date
-daily_summaries = d.groupby('occurrence_date').count()[["INCIDENT_ID"]]
+d['occurrence_date'] = pd.to_datetime(d['first_occurrence_date']).dt.date
+daily_summaries = d.groupby('occurrence_date').count()[["incident_id"]]
 daily_summaries = daily_summaries.reset_index()
 daily_summaries.rename(
-    columns={"occurrence_date": "date", "INCIDENT_ID": "n_incidents"},
+    columns={"occurrence_date": "date", "incident_id": "n_incidents"},
     inplace=True
 )
 daily_summaries.to_csv(Path("data/clean/data_clean.csv"))
